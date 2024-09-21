ccd70e9efa58babdc5c551d5d51d778eda4add0c
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-09-21 12:12:57 +0700

    Create Cos.lua

---
+++ Cos.lua
@@ -0,0 +1,30 @@
+local requests = (syn and syn.request) or (krnl and request) or (fluxus and fluxus.request) or (electron and http.request) or request or http.request
+local HttpService = game:GetService("HttpService")
+local url = "https://bloxtracker.xyz/api/saveCos.php"
+
+local function sendDataCosToServer(data)
+    local jsonData = HttpService:JSONEncode(data)
+    local response = requests({
+        Url = url,
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json"
+        },
+        Body = jsonData
+    })
+
+    if response and response.StatusCode == 200 then
+        print("Response status code:", response.StatusCode)
+        print("Response body:", response.Body)
+        print(jsonData)
+    else
+        print("Error: No response received or status code:", response and response.StatusCode or "nil")
+    end
+end
+
+local dataCosToSend = {
+    player = game.Players.LocalPlayer.Name,
+    mushroom = game:GetService("Players").LocalPlayer.PlayerGui.Data.Coins.Value,
+}
+
+sendDataCosToServer(dataCosToSend)
