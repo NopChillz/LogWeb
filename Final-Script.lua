ca8167d84a1fa31c48570ee44bf9b7e39ddabc33
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-10-01 10:52:45 +0700

    Update Final-Script.lua

--- Final-Script.lua
+++ Final-Script.lua
@@ -1,14 +1,17 @@
 getgenv().SettingsLog = {
     Show_Item_SettingsLog = {
     Log_Godhuman = true,  -- true or false
+    Log_SanguineArt = true,
     Log_Soul_Guitar = true,  -- true or false
-	  Log_Valkyrie_Helm = true,
+    Log_Valkyrie_Helm = true,
     Log_Cursed_Dual_Katana = true,
-	  Log_Shark_Anchor = true,
+    Log_Shark_Anchor = true,
+
     },
     Show_Material_SettingsLog = {
         Log_Mirror_Fractal = true,
     },
 }
-_G.PC = "FARM"
+_G.PC = "PC_NAME"
+_G.Key = "Key_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NopChillz/LogWeb/main/Script.lua"))()
