a07e67ea477dcb5e2a47603a5f3d725f9880606b
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-10-10 17:40:26 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -331,15 +331,15 @@ end
 function GetMeleeType()
     local CombatText = '' -- กำหนดค่าเริ่มต้นให้กับ CombatText
 
-    if getgenv().SettingsLog.Show_Item_SettingsLog["Log_Godhuman"] == true then
-        local Log_Godhuman = tonumber(game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyGodhuman",true))
-        if Log_Godhuman and Log_Godhuman == 1 then
-            CombatText = 'GOD'
-        end
-    elseif getgenv().SettingsLog.Show_Item_SettingsLog["Log_SanguineArt"] == true then
+    if getgenv().SettingsLog.Show_Item_SettingsLog["Log_SanguineArt"] == true then
         local Log_SanguineArt = tonumber(game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuySanguineArt",true))
         if Log_SanguineArt and Log_SanguineArt == 1 then
             CombatText = 'SGA'
+        end
+	elseif getgenv().SettingsLog.Show_Item_SettingsLog["Log_Godhuman"] == true then
+        local Log_Godhuman = tonumber(game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyGodhuman",true))
+        if Log_Godhuman and Log_Godhuman == 1 then
+            CombatText = 'GOD'
         end
     end
 
