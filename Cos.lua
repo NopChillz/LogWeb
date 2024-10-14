e1fbc84c0a0f62813e4dc07c8c37a32e9b9f32b2
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-10-15 01:21:07 +0700

    Update Cos.lua

--- Cos.lua
+++ Cos.lua
@@ -1,6 +1,3 @@
-task.spawn(function()
-repeat task.wait() until game:IsLoaded()
-wait(15)
 local requests = (syn and syn.request) or (krnl and request) or (fluxus and fluxus.request) or (electron and http.request) or request or http.request
 local HttpService = game:GetService("HttpService")
 local url = "https://bloxtracker.xyz/api/saveCos.php"
