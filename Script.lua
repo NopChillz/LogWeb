49ecc985e199ad598b770ea3cb1cbd38795c6484
NopChillz <82336260+NopChillz@users.noreply.github.com>
2026-03-25 21:46:21 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -5,7 +5,7 @@ repeat task.wait() until (game.Players.LocalPlayer.Neutral == false) == true
 
 local requests = (syn and syn.request) or (krnl and request) or (fluxus and fluxus.request) or (electron and http.request) or request or http.request
 local HttpService = game:GetService("HttpService")
-local url = "https://bloxtracker.xyz/api/saveData.php"
+local url = "https://bloxtracker.xyz/api/save-bf"
 
 function GetLogAllMeleeNew()
     combat = 0
